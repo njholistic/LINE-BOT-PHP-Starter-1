3af1c168ad60cf30a940685c82889b80a5535569
njholistic <njholistic@gmail.com>
2017-10-27 13:32:10 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@ $access_token = '8ba20e5d48c1fa5c4bd6d316ea50244a';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . 8ba20e5d48c1fa5c4bd6d316ea50244a);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
