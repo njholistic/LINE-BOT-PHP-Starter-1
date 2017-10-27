47100f03b3b6aad93956d8a14ef31e360310cdf8
njholistic <njholistic@gmail.com>
2017-10-27 13:28:32 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = '8ba20e5d48c1fa5c4bd6d316ea50244a';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
